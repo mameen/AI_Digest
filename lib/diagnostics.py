ab727cc63ba61d97fa432bc76899da55f2ee6747
Mohamed Ameen <mohamed_ameen@hotmail.com>
2026-07-31 08:44:10 -0700

    skeleton

--- lib/diagnostics.py
+++ lib/diagnostics.py
@@ -1,6 +1,8 @@
 """Stub: re-export from llm_pipeline.diagnostics until T2 extracts to lib/diagnostics.py."""
 
 from llm_pipeline.diagnostics import (  # type: ignore[import-untyped]
+    _render_run_log,
+    _render_waterfall_html,
     finish_collector,
     get_collector,
     init_collector,
@@ -8,4 +10,12 @@ from llm_pipeline.diagnostics import (  # type: ignore[import-untyped]
     log,
 )
 
-__all__ = ["finish_collector", "get_collector", "init_collector", "instrumented_llm_call", "log"]
+__all__ = [
+    "_render_run_log",
+    "_render_waterfall_html",
+    "finish_collector",
+    "get_collector",
+    "init_collector",
+    "instrumented_llm_call",
+    "log",
+]
